1dce7df86c5fb7d9ef7f60d7dddbfbd1a0eae6e1
Jinyang Li <jl163@gpu-24-7.local>
2014-12-21 16:32:03 -0500

    stuff

--- Makefile
+++ Makefile
@@ -1,9 +1,16 @@
-all: mem mpitest
+all: mem 
 
-mem: mem.cu
-	nvcc mem.cu -I${HOME}/pkg/mvapich2/include -L${HOME}/pkg/mvapich2/lib -lmpi -o mem 
+mem.o: mem.cu
+	nvcc -c mem.cu 
+
+main.o: main.cc
+	mpicc -c main.cc
+
+mem : mem.o main.o
+	mpicc main.o mem.o -L/share/apps/cuda/6.5.12/lib64 -lcudart
+
 mpitest: mpitest.cc
-	nvcc mpitest.cc -I${HOME}/pkg/mvapich2/include -L${HOME}/pkg/mvapich2/lib -lmpi -o mpitest
+	nvcc mpitest.cc -lmpi -o mpitest
 
 clean:
 	rm -f mem mpitest
--- mem.cu
+++ mem.cu
@@ -3,7 +3,13 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
-#include <mpi.h>
+
+
+#include "mem.h"
+#include "timediff.h"
+
+extern int debug;
+
 
 __global__  void kernel(float *array, int n, int stride)
 {
@@ -21,105 +27,17 @@ __global__  void kernel(float *array, int n, int stride)
 	cuPrintf("n %d stride %d my_id %d start %d end %d array[0]=%f\n", n, stride, index, start, end, array[0]);
 }
 
-int
-timediff(struct timespec *t0, struct timespec *t1)
-{
-	return (t1->tv_sec - t0->tv_sec)*1000000 + (t1->tv_nsec - t0->tv_nsec)/1000;
-}
-
-int main(int argc, char **argv)
-{
-	bool debug = 1;
-	int num_elements = 16;
-	int n_tblk = 1;
-	int nt_tblk = 1;
-	char c;
+void launch_kernel(int n_tblk, int nt_tblk, float *device, int n)
+{	
 	struct timespec t0, t1;
 
-	while (( c = getopt(argc, argv, "d:n:b:t:")) != -1) {
-		switch (c) 
-		{
-			case 'd':
-				debug = atoi(optarg);
-				break;
-			case 'n':
-				if (num_elements > 250) {
-					num_elements = 250;
-					printf("Capped to 250M elements!!!!\n");
-				}
-				num_elements = atoi(optarg) * 1000000;
-				break;
-			case 'b':
-				n_tblk= atoi(optarg);
-				break;
-			case 't':
-				nt_tblk= atoi(optarg);
-				break;
-			default:
-				printf("valid options: -n <size> -b <# thread blocks> -t <# threads per block> -d <debug>\n");
-				exit(1);
-			}
-	}
+	if (debug) cudaPrintfInit(); // initialize cuPrintf
 
-	printf("Array size:%dM, ThreadBlocks:%d, ThreadsPerBlock: %d Total Threads %d\n", \
-			num_elements/1000000, n_tblk, nt_tblk, n_tblk * nt_tblk);
-
-	int myrank;
-	MPI_Status status;
-	int tag = 999;
-
-	MPI_Init(&argc, &argv);
-	MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
-
-	float *device_array = 0;
-	float *host_array = 0;
-
-	// malloc host memory
-	host_array = (float *)malloc(num_elements * sizeof(float));
-	for (int i = 0; i < num_elements; i++) {
-		host_array[i] = i + 4.0;
-	}
-
-	// cudaMalloc device memory
-	cudaMalloc((void**)&device_array, num_elements * sizeof(float));
-
-	// zero out the device array with cudaMemset
-	cudaMemset(device_array, 0, num_elements * sizeof(float));
-
-	if (myrank == 0 ) {
-		// copy the contents of the device array to the host
-		clock_gettime(CLOCK_REALTIME, &t0);
-		cudaMemcpy(device_array, host_array, num_elements * sizeof(float), cudaMemcpyHostToDevice);
-		cudaDeviceSynchronize();
-		clock_gettime(CLOCK_REALTIME, &t1);
-		printf("H->D Transfer %d usec\n", timediff(&t0, &t1));
-
-		if (debug) {
-			cudaPrintfInit(); // initialize cuPrintf
-		}
-
-
-		clock_gettime(CLOCK_REALTIME, &t0);
-		kernel<<<n_tblk,nt_tblk>>>(device_array, num_elements, num_elements/(n_tblk*nt_tblk));
-		cudaDeviceSynchronize();
-		clock_gettime(CLOCK_REALTIME, &t1);
-
-		printf("Kernel finished in %d usec %f\n", timediff(&t0, &t1), host_array[0]);
-
-		clock_gettime(CLOCK_REALTIME, &t0);
-		cudaMemcpy(host_array, device_array, num_elements * sizeof(float), cudaMemcpyDeviceToHost);
-		cudaDeviceSynchronize();
-		clock_gettime(CLOCK_REALTIME, &t1);
-		printf("D->H Transfer %d usec\n", timediff(&t0, &t1));
-
-		MPI_Send(device_array, num_elements, MPI_FLOAT, 1, tag, MPI_COMM_WORLD);
-		printf("MPI sent\n");
-	}else {
-		MPI_Recv(host_array, num_elements, MPI_FLOAT, 0, tag, MPI_COMM_WORLD, &status);
-		int count;
-		MPI_Get_count(&status, MPI_FLOAT, &count); 
-		printf("MPI received %d floats %f\n", count, host_array[0]);
-	}
+	clock_gettime(CLOCK_REALTIME, &t0);
+	kernel<<<n_tblk,nt_tblk>>>(device, n, n/(n_tblk*nt_tblk));
+	cudaDeviceSynchronize();
+	clock_gettime(CLOCK_REALTIME, &t1);
+	printf("Kernel finished in %d usec\n", TIME_DIFF(t0, t1));
 
 	if (debug) {
 		// display the device's greeting
@@ -129,19 +47,35 @@ int main(int argc, char **argv)
 		cudaPrintfEnd();
 	}
 
-	if (debug) {
-		for(int i = 0; i < (num_elements<10?num_elements:10); ++i)
-			printf("%f ", host_array[i]);
-		printf("\n");
-	}
-
-	// use free to deallocate the host array
-	free(host_array);
-
-	// use cudaFree to deallocate the device array
-	cudaFree(device_array);
-
-	MPI_Finalize();
-
-	return 0;
 }
+
+void alloc_device_mem(float **device_array, int n)
+{
+	// cudaMalloc device memory
+	cudaMalloc(device_array, n* sizeof(float));
+
+	// zero out the device array with cudaMemset
+	cudaMemset(*device_array, 0, n* sizeof(float));
+
+}
+
+
+void transfer_mem(float *device, float *host, int n, bool host2dev)
+{
+	struct timespec t0, t1;
+
+	clock_gettime(CLOCK_REALTIME, &t0);
+	if (host2dev) 
+		cudaMemcpy(device, host, n* sizeof(float), cudaMemcpyHostToDevice);
+	else
+		cudaMemcpy(host, device, n* sizeof(float), cudaMemcpyDeviceToHost);
+	cudaDeviceSynchronize();
+	clock_gettime(CLOCK_REALTIME, &t1);
+	printf("%s Transfer took %d usec\n", host2dev?"H->D":"D->H", TIME_DIFF(t0, t1));
+}
+
+void free_device_mem(float *device)
+{
+	cudaFree(device);
+}
+
